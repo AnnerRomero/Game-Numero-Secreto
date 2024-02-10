eb57396f7355e9c509b9cb9d39e4e5e7ea741455
Anner Romero <romeroanner16@gmail.com>
2024-02-09 20:13:31 -0600

    Conectar js con html atravez de el DOM (Document Odject Model)

---
+++ app.js
@@ -0,0 +1,6 @@
+let elementoH1 = document.querySelector('h1');
+elementoH1.innerHTML = "Juego de el numero secreto";
+
+let elementoP = document.querySelector('p');
+elementoP.innerHTML = "Digite un numero entre 1 y 10";
+
