c8968608d4b86fb26efd09b4d7a93d624ffe3b5a
Anner Romero <romeroanner16@gmail.com>
2024-02-09 20:51:09 -0600

    Cree funciones para optimizar el codigo

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-let elementoH1 = document.querySelector('h1');
-elementoH1.innerHTML = "Juego de el numero secreto";
+
+/*
+let elementoP = document.querySelector('p');
+elementoP.innerHTML = "Digite un numero entre 1 y 10";
 
 let elementoP = document.querySelector('p');
 elementoP.innerHTML = "Digite un numero entre 1 y 10";
 
+Este codigo necesitaba ser optimizado, por ello se crea las funcion donde se reduce el codigo
+*/
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
+
+asignarTextoElemento('h1', 'Juego de el numero');
+asignarTextoElemento('p', 'Elija un numero entre 1 y 20')
