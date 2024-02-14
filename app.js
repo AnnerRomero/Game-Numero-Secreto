92324d4ef8d4eb470137c449db6885fdcfec8a38
Anner Romero <romeroanner16@gmail.com>
2024-02-13 19:38:12 -0600

    Cree la funcion que genera el numero secreo para nuestro juego

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-//Cree la funcion "intentoDeUsuario", colocando el comando "oneclik"; en html
-// dentro de las comillas "intentoDeUsuario();" para conectarlo con js.
+//LA Variable numeroSecreto afuera de una funcion es de alcance global y dentro de una funcion es solo de bloque
+let numeroSecreto = generaNumeroSecreto();
+
+console.log (numeroSecreto);
+
 function intentoDeUsuario(){
-    alert("Click desde el boton")
+    alert("Click desde el boton");
+    return;
 }
 
 
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
 }
 
 asignarTextoElemento('h1', 'Juego de el numero');
 asignarTextoElemento('p', 'Elija un numero entre 1 y 20')
+
+//Funcion para generar el numero secreto
+
+function generaNumeroSecreto () {
+    return Math.floor(Math.random()*10)+1;
+  
+    
+}
