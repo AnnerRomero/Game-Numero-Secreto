722a2cca14c14aa86b6bd73995ee25b5eb7bccc9
Anner Romero <romeroanner16@gmail.com>
2024-02-22 12:57:32 -0600

    Cree la funicon limpiarCaja y habilite el boton reiniciar juego

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ function asignarTextoElemento(elemento, texto){
     return;
 }
 
-asignarTextoElemento('h1', 'Juego de el numero');
-asignarTextoElemento('p', 'Elija un numero entre 1 y 20')
+asignarTextoElemento('h1', 'Juego de el numero secreto');
+asignarTextoElemento('p', 'Elija un numero entre 1 y 10');
 
 //Funcion para generar el numero secreto
 
@@ -22,22 +22,32 @@ function generaNumeroSecreto () {
 
 // Funcion para captur el imput(es decir lo que el usuario ingreso, la entrada) (se conecta con el input de HTML)
 //Se conecto atravez de el ID de la etiqueta input de HTML son el comando "document.getElemenById ()"
+
 function verificarIntento(){
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value); 
 
      console.log(intentos);
+
     if(numeroDeUsuario === numeroSecreto) {
-        asignarTextoElemento('p', `Acertaste en ${intentos} ${(intentos === 1) ? 'vez':'veces'} `); // Esta linea es una condicion con operador ternadio, que solo va a funcionar con las comillas ``
-    } else {
-        if (numeroDeUsuario < numeroSecreto){
-            asignarTextoElemento('p','El numero secreto es mayor');
-        }else {
+        asignarTextoElemento('p', `Acertaste en ${intentos} ${(intentos === 1) ? 'vez':'veces'} `); 
+        document.getElementById('reiniciar').removeAttribute('disabled'); //Esta linea remueve el atributo disable en el boton reiniciar en html, para poder habilitar el boton.
+
+    }else{
+        if (numeroDeUsuario > numeroSecreto){
             asignarTextoElemento('p','El numero secreto es menor');
+        }else {
+            asignarTextoElemento('p','El numero secreto es mayor');
         }
         intentos++;
+        
+        limpiarCaja();
     }
    
     return;
 }
 
+//Funcion para limpiar la caja donde el usuario ingresa el numero depues de fallar cada intento
+function limpiarCaja (){
+    document.querySelector('#valorUsuario').value = '';
+}
 
